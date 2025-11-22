6a7f27ad307757899c5191e33fdb289b2e124bbd
Okeha <okehanthony1234@gmail.com>
2025-11-22 08:38:40 +0000

    chore: Updated pyproject.toml dependencies

--- src/model/finetune/dataset.py
+++ src/model/finetune/dataset.py
@@ -1,12 +1,27 @@
 # Dataset should load data from the data json file.
-# this should load up the dataset from a json file, split the video into tensor frames, and return a list of dictionaries with input_ids, attention_mask, pixel_values, and labels
+# this should load up the dataset from a json file, split the dataset into train and tests, then load video into tensor frames, and return a list of dictionaries with input_ids, attention_mask, pixel_values, and labels
+import json
+import yaml
+with open("../params/vlm.yml", "r") as f:
+    params = yaml.safe_load(f)
 
+
+dataset_path = params["dataset_path"]
 
 class DatasetLoader():
 
     def __init__(self, verbose=True, dataset_file="wlasl_cleaned.json"):
         self.verbose = verbose
-        self.dataset_file = dataset_file
-        self.dataset = []
-        self.load_dataset()
+        with open(dataset_path, "r") as f:
+            self.dataset = json.load(f)
+
+        print(f"Loaded dataset with {len(self.dataset)} samples from {dataset_path}")
         pass
+
+
+
+
+
+if __name__ == "__main__":
+    dataset_loader = DatasetLoader()
+    print(dataset_path)
